eb909247fc7e4bbdb47feeea679a01a14b862c5f
Som Pourfarzaneh <doryoku@gmail.com>
2022-12-14 11:48:58 -0800

    Initial commit.

---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public NetworkManager NetworkManager;
+
+    private void Start()
+    {
+        NetworkManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+    }
+
+    public void OnClick()
+    {
+        NetworkManager.SendMessage("draw");
+    }
+
+}
